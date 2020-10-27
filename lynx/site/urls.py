6d3f35f928f9003cd8f04725e88c7741c2fdd018
Stuart MacKay <smackay@flagstonesoftware.com>
2020-10-27 21:14:37 +0000

    Add redirect from / to the news page

--- lynx/site/urls.py
+++ lynx/site/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/news/')),
     path('account/', include('allauth.urls')),
     path("admin/", admin.site.urls),
     path('news/', include('lynx.news.urls')),
